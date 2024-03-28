b799eb0f94a8b2745b2ac36a3819197989c160c2
Shahin Amon <amons@bzz.ch>
2024-03-28 12:36:07 +0100

    Add client-side code and fix import in PasswordInput component Co-authored-by: Ben Brändle <BWizard06@users.noreply.github.com>

--- components/PasswordInput.js
+++ components/PasswordInput.js
@@ -2,16 +2,14 @@
 import React from "react";
 import {EyeSlashIcon, EyeIcon} from '@heroicons/react/24/outline';
 import {useState} from "react";
-import {input} from '@nextui-org/react';
+import {Input} from '@nextui-org/react';
 
 const PasswordInput = () => {
     const [slashed, setSlashed] = useState(true);
     return (
         <Input
             placeholder='Passwort'
-            startContent={
-                <button className=''
-            }
+            type={slashed ? 'password' : 'text'}
         />
 
     );
